f0f032184aa73aa470316329546a5330fbb56b0e
Kendall Malik Howard <kendallhoward04@gmail.com>
2023-07-27 15:47:13 -0700

    signup and login

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -15,12 +15,27 @@ function LoginForm() {
 
     const [errorMessage, setErrorMessage] = useState(null)
 
-    async function handleSubmit(e) {
-        e.preventDefault()
-       
+async function handleSubmit(e) {
+    const response = await fetch(`http://localhost:5001/authentication/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
 
+    const data = await response.json()
+
+    if (response.status === 200) {
+        setCurrentUser(data.user)
+        history.push(`/`)
+    } else {
+        setErrorMessage(data.message)
     }
+}
 
+
+    
     return (
         <main>
             <h1>Login</h1>
@@ -63,6 +78,6 @@ function LoginForm() {
             </form>
         </main>
     )
-}
 
+        }
 export default LoginForm
