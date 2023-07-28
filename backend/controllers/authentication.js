14c4cea1f6db53edf711ee1db30cfba5a25d9b43
Kendall Malik Howard <kendallhoward04@gmail.com>
2023-07-27 20:02:58 -0700

    Sessions

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -7,9 +7,7 @@ const { User } = db
 router.post('/', async (req, res) => {
     
     let user = await User.findOne({
-        where: { 
-            email: req.body.email          
-        }
+        where: { email: req.body.email }
     })
 
     if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
@@ -17,11 +15,34 @@ router.post('/', async (req, res) => {
             message: `Could not find a user with the provided username and password` 
         })
     } else {
-        res.json({ user })
+        req.session.userID = user.userId
+        res.json({ user })                                       
     }
 })
 
+router.get('/profile', async (req, res) => {
+    console.log(req.session.userId)
+    try {
+        let user = await User.findOne({
+            where: {
+                userId: req.session.userId                    
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
+    }
+})
 
+router.post('/super-important-route', async (req, res) => {
+    if(req.session.userId){
+        console.log('Do the really super important thing')
+        res.send('Done')
+    } else {
+        console.log('You are not authorized to do the super important thing')
+        res.send('Denied')
+    }
+})
 
 
 
